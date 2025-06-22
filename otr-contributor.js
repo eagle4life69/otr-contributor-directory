c4867496f94329cf799a741b61f51f5364b14913
eagle4life69 <contact@eagle4life69.com>
2025-06-22 16:57:10 -0700

    Update otr-contributor.js

--- otr-contributor.js
+++ otr-contributor.js
@@ -3,3 +3,11 @@ function showTab(index) {
     el.style.display = i === index ? "block" : "none";
   });
 }
+
+function showYearTab(showIndex, yearIndex) {
+  document.querySelectorAll(`.year-content.tab-${showIndex}-year-*`).forEach(el => {
+    el.style.display = "none";
+  });
+  const el = document.querySelector(`.year-content.tab-${showIndex}-year-${yearIndex}`);
+  if (el) el.style.display = "block";
+}
